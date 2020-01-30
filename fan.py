88cfbc48d7379cea8e736df31c32d5de7315da3c
edgeboyo <mkolybko@gmail.com>
2020-01-30 16:50:20 +0000

    Added fan test

---
+++ fan.py
@@ -0,0 +1,18 @@
+import os, time
+
+os.system("sudo pigiod")
+time.sleep(1)
+import pigpio
+
+FAN1 = #Connect fan to this
+
+pi = pigpio.pi()
+pi.set_servo_pulsewidth(FAN1, 1000)
+time.sleep(5)
+pi.set_servo_pulsewidth(FAN1, 1600)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    os.system("sudo killall pigpiod")
